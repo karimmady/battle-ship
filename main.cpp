c2c6d96f447741efe65669a97c3b6768af9e318f
karimmady <karimmady5@hotmail.com>
2016-12-11 16:21:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,101 +1,63 @@
-#ifndef Menu_hpp
-#define Menu_hpp
-
-#include<iostream>
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
+#include <random>
+#include <ctime>
+#include "Menu.h"
+#include "Grid.h"
+#include "Boats.h"
 using namespace std;
 
-class Menu
-{
-public:
-	Menu();
-	void setMenu(float width, float height, sf::RenderWindow &window);
-	void draw(sf::RenderWindow &window);
-	void background(sf::RenderWindow &window);
-	void mouse(sf::Vector2i, sf::RenderWindow &window, bool &bisho);
-private:
-	sf::Font font;
-	sf::Text txt[4];
-	sf::FloatRect b[4];
-	sf::Texture back;
-	sf::Vector2f point;
-};
+int main() {
+	srand(time(NULL));
+	sf::RenderWindow window(sf::VideoMode(1000, 600), "Battle ship");
 
-Menu::Menu()
-{
-	if (!font.loadFromFile("STENCIL.TTF"))
+	Menu m;
+	m.setMenu(1000, 600, window);
+
+	Grid g;
+	g.setGrid(window);
+	Boats b;
+	b.inBoats();
+
+	bool bisho = false;
+
+	while (window.isOpen())
 	{
-		cout << "Error\n";
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+			{
+				window.close();
+			}
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+				m.mouse(sf::Mouse::getPosition(window), window, bisho);
+			}
+		}
+		window.clear();
+		//m.background(window);
+		if (!bisho)
+		{
+			m.background(window);
+			m.draw(window);
+		}
+		else {
+			g.draw(window);
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+				b.drag(window);
+			}
+			
+
+		}
+
+		window.display();
 	}
+	return EXIT_SUCCESS;
 }
-
-void Menu::setMenu(float width, float height, sf::RenderWindow &window) {
-	txt[0].setFont(font);
-	txt[0].setColor(sf::Color::White);
-	txt[0].setString("Play");
-	txt[0].setPosition(sf::Vector2f(width / 16, height / 1.3));
-
-	txt[1].setFont(font);
-	txt[1].setColor(sf::Color::White);
-	txt[1].setString("Highscore");
-	txt[1].setPosition(sf::Vector2f(width / 3.5, height / 1.3));
-
-	txt[2].setFont(font);
-	txt[2].setColor(sf::Color::White);
-	txt[2].setString("Instructions");
-	txt[2].setPosition(sf::Vector2f(width / 1.7, height / 1.3));
-
-	txt[3].setFont(font);
-	txt[3].setColor(sf::Color::White);
-	txt[3].setString("Exit");
-	txt[3].setPosition(sf::Vector2f(width / 1.103, height / 1.3));
-
-	b[0] = txt[0].getGlobalBounds();
-	b[1] = txt[1].getGlobalBounds();
-	b[2] = txt[2].getGlobalBounds();
-	b[3] = txt[3].getGlobalBounds();
-	point = sf::Vector2f(b[3].left + 1.f, b[3].top + 1.f);
-}
-
-
-void Menu::draw(sf::RenderWindow &window)
-{
-	for (int i = 0; i < 4; i++)
-	{
-		window.draw(txt[i]);
-	}
-}
-
-void Menu::background(sf::RenderWindow &window)
-{
-	if (!back.loadFromFile("Battle_ship_Background.jpg"))
-		cout << "failed";
-	sf::Sprite sprite(back);
-	sprite.setPosition(0, 0);
-	sprite.setScale(0.85, 0.6);
-	window.draw(sprite);
-}
-void Menu::mouse(sf::Vector2i pos, sf::RenderWindow &window, bool &bisho)
-{
-	sf::Vector2f f;
-	f.x = pos.x;
-	f.y = pos.y;
-	if (txt[0].getGlobalBounds().contains(f))
-	{
-		bisho = true;
-	}
-
-	if (txt[1].getGlobalBounds().contains(f))
-	{
-	}
-	if (txt[2].getGlobalBounds().contains(f))
-	{
-	}
-	if (txt[3].getGlobalBounds().contains(f))
-	{
-		exit(0);
-	}
-}
-#endif /* Menu_hpp */
